afa78879d0b88ecf4b0b28b2c23c4b792d43bcf0
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2023-04-11 18:31:21 +0100

    Added collectables
    Added hearts, stars and camera scroll.

--- levels.py
+++ levels.py
@@ -1,14 +1,14 @@
 import pygame as p
 
-test = [[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+test = [[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 0, 0, 0, 0],
         [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]]
 for _ in range(5):
     test.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
@@ -28,9 +28,14 @@ def drawText(screen, player):
     screen.blit(livesText, p.Rect(25, 50, 0, 0))
 
 
-def drawLevel(screen, lvl, images, blockDimensions, offsets):
+def drawLevel(screen, lvl, player, images, blockDimensions, offsets):
     blockWidth, blockHeight = blockDimensions
     xOffset, yOffset = offsets
+
+    # Camera Scroll
+    if player.position["c"] > 25:
+        xOffset -= (player.position["c"] - 25) * blockWidth
+
     for r in range(len(lvl)):
         for c in range(len(lvl[r])):
             if lvl[r][c] == 1:
@@ -40,13 +45,20 @@ def drawLevel(screen, lvl, images, blockDimensions, offsets):
                                    blockWidth, blockHeight))
 
 
-def drawSprites(screen, player, images, blockDimensions, offsets):
+def drawSprites(screen, lvl, player, images, blockDimensions, offsets):
     blockWidth, blockHeight = blockDimensions
     xOffset, yOffset = offsets
+    playerXOffset = xOffset
+
+    # Camera Scroll
+    if player.position["c"] > 25:
+        xOffset -= (player.position["c"] - 25) * blockWidth
+
+    # Player
     if player.orientation == "right":
         screen.blit(images["player"],
-                    p.Rect((player.position["c"] * blockWidth) + xOffset,
-                           (player.position["r"] * blockHeight) + yOffset,
+                    p.Rect((player.position["c"] * blockWidth + xOffset),
+                           (player.position["r"] * blockHeight + yOffset),
                            blockWidth, blockHeight))
     else:
         screen.blit(p.transform.flip(images["player"], True, False),
@@ -54,10 +66,28 @@ def drawSprites(screen, player, images, blockDimensions, offsets):
                            (player.position["r"] * blockHeight) + yOffset,
                            blockWidth, blockHeight))
 
+    # Stars
+    for r in range(len(lvl)):
+        for c in range(len(lvl[r])):
+            if lvl[r][c] == 2:
+                screen.blit(images["star"],
+                            p.Rect((c * blockWidth) + xOffset,
+                                   (r * blockHeight) + yOffset,
+                                   blockWidth, blockHeight))
+
+    # Hearts
+    for r in range(len(lvl)):
+        for c in range(len(lvl[r])):
+            if lvl[r][c] == 3:
+                screen.blit(images["heart"],
+                            p.Rect((c * blockWidth) + xOffset,
+                                   (r * blockHeight) + yOffset,
+                                   blockWidth, blockHeight))
+
 
 def drawGame(screen, lvl, player, images, blockWidth, blockHeight):
     xOffset = blockWidth * 5
     yOffset = blockHeight * 5
-    drawLevel(screen, lvl, images, [blockWidth, blockHeight], [xOffset, yOffset])
-    drawSprites(screen, player, images, [blockWidth, blockHeight], [xOffset, yOffset])
+    drawLevel(screen, lvl, player, images, [blockWidth, blockHeight], [xOffset, yOffset])
+    drawSprites(screen, lvl, player, images, [blockWidth, blockHeight], [xOffset, yOffset])
     drawText(screen, player)
